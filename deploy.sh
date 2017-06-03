e1bcf5b5c0db00ee4961fdde2e0bb2b0ef9bd389
Ubuntu <ubuntu@ip-172-31-17-228.ap-northeast-1.compute.internal>
2017-06-03 08:05:59 +0000

    chouchin anko version

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+rm myproject.sock
+sudo systemctl stop myproject
+sudo systemctl start myproject
+sudo systemctl enable myproject
--- myproject.py
+++ myproject.py
@@ -2,14 +2,33 @@
 # -*- coding: utf-8 -*-
 # vim:set fileencoding=utf-8:
 
-from flask import Flask
-
+from flask import Flask, request
+import requests
+import boto3
 app = Flask(__name__)
 
 @app.route("/") #トップディレクトリ / は別に使うため
+ #def hello():
+ #    # return "{\"name\":\"goechan\"}"
+ #    return "<h1 style='color:blue'>Hello There!</h1>"
 def hello():
-    return "{\"name\":\"goechan\"}"
-    # return "<h1 style='color:blue'>Hello There!</h1>"
+    return '<form action="/getAttribute" method="GET"><input name="url"><input type="submit" value="Search"></form>'
+
+@app.route("/getAttribute")
+def echo(): 
+    session = boto3.Session(profile_name='default')
+    rek = session.client('rekognition')
+    
+    resp = requests.get(request.args.get('url', ''))
+    #resp = requests.get('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEpvRc3Td6OFWApLfYBdMgJ9SGbKpfIrhyTJxmScxF_s1LP28Ihg')
+    imgbytes = resp.content
+    
+    # return "You said: " + request.args.get('text', '')
+    rekresp = rek.detect_labels(Image={'Bytes': imgbytes})
+    
+    # print(rekresp)
+    # return "You said: " + request.args.get('text', '')
+    return str(rekresp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
